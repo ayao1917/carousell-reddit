c8f6ceb18641cc0028771c63c4461ec65c0eb048
ayao <ayao@moker.com>
2019-01-09 11:18:49 +0800

    add user's topics

---
+++ pages/user/topics.vue
@@ -0,0 +1,92 @@
+<template>
+  <b-container class="mt-3">
+    <b-row>
+      <b-col cols="10">
+        <h3>My Topics</h3>
+      </b-col>
+      <b-col>
+        <b-btn size="small" variant="primary" block @click="showAddModal">Add</b-btn>
+      </b-col>
+    </b-row>
+    <b-table class="mt-3" striped hover :items="topics" :fields="fields" v-if="topics.length !== 0">
+      <template slot="actions" slot-scope="row">
+        <b-button size="sm" @click.stop="showEditModal(row.item, row.index, $event.target)" class="mr-1">
+          Edit
+        </b-button>
+      </template>
+    </b-table>
+    <b-jumbotron header="No Topics Found" v-if="topics.length === 0">
+      <p>Click here to add one</p>
+      <b-btn variant="primary" @click="showAddModal">Add</b-btn>
+    </b-jumbotron>
+
+    <b-modal ref="topicModalRef" hide-footer title="Add Topic">
+      <div class="d-block text-center">
+        <b-form>
+          <b-form-group label="Content"
+                        label-for="contentInput">
+            <b-form-textarea id="contentInput"
+                             v-model="topicForm.content"
+                             placeholder="Enter something"
+                             :rows="3"
+                             :max-rows="6">
+            </b-form-textarea>
+          </b-form-group>
+        </b-form>
+      </div>
+      <b-btn class="mt-5" variant="outline-primary" block @click="doAdd" v-if="modalType === 'add'">Add</b-btn>
+      <b-btn class="mt-5" variant="outline-primary" block @click="doEdit" v-if="modalType === 'edit'">Update</b-btn>
+      <b-btn class="mt-3" variant="outline-danger" block @click="hideModal">Cancel</b-btn>
+    </b-modal>
+  </b-container>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: "topics",
+    data: () => {
+      return {
+        fields: [
+          'content',
+          { key: 'votes', label: 'Votes' },
+          { key: 'actions', label: 'Actions' }
+        ],
+        topicForm: {
+          content: ''
+        },
+        modalType: 'add',
+      }
+    },
+    computed:{
+      ...mapGetters({
+        topics: 'topic/getTopics',
+      }),
+    },
+    methods: {
+      showAddModal() {
+        this.modalType = 'add';
+        this.$refs.topicModalRef.show();
+      },
+      showEditModal(item) {
+        this.modalType = 'edit';
+        this.topicForm = item;
+        this.$refs.topicModalRef.show();
+      },
+      hideModal() {
+        this.$refs.topicModalRef.hide();
+      },
+      doAdd() {
+      },
+      doEdit() {
+      },
+    },
+    mounted() {
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
